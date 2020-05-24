e0a8f5f96999c7c21a1118f69345f2a80fd5c2a7
Roger Bagué <rogerbague@gmail.com>
2020-05-25 00:11:20 +0200

    add terser to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,29 @@
 import babel from "@rollup/plugin-babel"
+import { terser } from "rollup-plugin-terser"
+
+const output = {
+  name: 'StimulusCheckAll',
+  format: 'umd',
+  sourcemap: true,
+  globals: {
+    stimulus: 'Stimulus',
+    '@github/check-all': 'checkAll'
+  }
+};
 
 export default {
   input: 'src/index.js',
-  output: {
-    name: 'StimulusCheckAll',
-    file: 'dist/stimulus-check-all.umd.js',
-    format: 'umd',
-    sourcemap: true,
-    globals: {
-      stimulus: 'Stimulus',
-      '@github/check-all': 'checkAll'
+  output: [
+    {
+      ...output,
+      file: 'dist/stimulus-check-all.js',
+    },
+    {
+      ...output,
+      file: 'dist/stimulus-check-all.min.js',
+      plugins: [terser()]
     }
-  },
+  ],
   external: ['stimulus', '@github/check-all'],
   plugins: [babel({ babelHelpers: 'bundled' })]
 }
