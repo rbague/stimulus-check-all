5d03baadba90fbb49e519896a8530aead64b7489
Donnadonnam <123767522+Donnadonnam@users.noreply.github.com>
2023-02-14 03:26:55 -0600

    Donnadonnamrollup.config.js - stimulus --check-all [G...github.dev

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,22 @@
-import babel from '@rollup/plugin-babel'
+import babel from '@rollup/plugin-babel',{file: 'dist/stimuluapp.use(express.static('public'))}
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/stimulus-check-all.js',
-    format: 'cjs',
-    sourcemap: true,
-    globals: {
-      stimulus: 'Stimulus',
-      '@github/check-all': 'checkAll'
+   file: 'dist/stimulus/check-all.js',
+    output: {' from: cjs',
+    sourcemap:
+    express = require('express')
+    const router = express.Router()
+    
+    uter.get((req,res)=>{
+     res.send(http-response)
+    })
+    
+    module.exports = router,
+    globals: { output: 'stimulus': true ,'stimulus': true,
+    '@github/(check-all':
+    ('checkAll')
     }
   },
   external: ['stimulus', '@github/check-all'],
